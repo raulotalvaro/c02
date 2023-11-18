2d40278d76357df7e459f52108ecaca6aee9ac04
raul otalvaro <raulotalvaro@gmail.com>
2023-11-18 16:48:21 +0100

    dos

--- ex01/ft_strcmp.c
+++ ex01/ft_strcmp.c
@@ -1,5 +1,30 @@
+#include <unistd.h>
+char    *ft_strcpy(char *dest, char *src)
+{
+    int i;
+
+    i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
 int main(int a, char *args[]){
-    if(a != 2)return;
-    char p=args[1];
-    write(1, &p,1);
+    if(a != 2)return 0;
+    char *p=args[1];
+    char *q=args[2];
+    q=ft_strcpy(p, q);
+    int i;
+
+    i = 0;
+    int s;
+    while (q[i] != '\0')
+    {   
+        s = p[i];
+        write(1, &s,1);
+        i++;
+    }  
 }
