681cea9ee25e5304342028939a04f456bd0d8a7d
raul otalvaro <raulotalvaro@gmail.com>
2023-11-18 18:56:12 +0100

    2

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -1,32 +1,21 @@
-/*#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>*/
+#include <unistd.h>
 //los caracteres alfabeticos en ascci van del 65 al 90 (mayusculas) y del 65+32=97 al 90+32=122 (minusculas)
-char    *ft_str_is_alpha(char *dest, char *src, unsigned int n)
+char    ft_str_is_alpha(char *str)
 {
     int i;
+
     i = 0;
-    while (src[i]!=0)
+    while (str[i] != 0)
     {    
-        if()
-        i=i+1;
-    }
-    dest[i] = 0;
-    return (dest);
-}
-/*int main(int a, char *args[]){
-     if(a!=4)return 0;
-    char *dest=args[1];
-    char *src=args[2];
-    int n = atoi(args[3]);
-    ft_str_is_alpha(dest, src, n);
-    int i;
-    i = 0;
-    int s;
-    while (dest[i] != '\0')
-    {   
-        s = dest[i];
-        write(1, &s,1);
+        if(!(65<=str[i]<=90 && 97<=str[i]<=122))
+            return (0);
         i++;
-    }  
-}*/
+    }
+    return (1);
+}
+int main(int a, char *args[]){
+    if(a!=2)return 0;
+    char *str=args[1];   
+    int i=ft_str_is_alpha(str);
+    write(1, &i,1);
+}
